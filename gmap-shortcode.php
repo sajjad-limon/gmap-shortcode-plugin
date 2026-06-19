b98f2ce49220297a56bc206d03d5f2310117a680
Sajjad Hossen Limon <122174379+sajjad-limon@users.noreply.github.com>
2026-06-19 12:20:26 +0600

    initial setup

---
+++ gmap-shortcode.php
@@ -0,0 +1,38 @@
+<?php 
+/**
+ * Plugin Name:       Easy Google Map Shortcode
+ * Plugin URI:        https://github.com/sajjad-limon
+ * Description:       Lightweight, and simple shortcode plugin for display google maps on your website via shortcode in any pages.
+ * Version:           1.0.0
+ * Author:            Sajjad Limon
+ * Author URI:        https://github.com/sajjad-limon
+ * Text Domain:       easy_gmap
+ * Requires PHP:      7.4
+ * Requires at least: 6.2
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+function easy_gmap_register_shortcode($attributes){
+    
+    $default_value = [
+        'place' => 'New York, USA',
+        'width' => 800,
+        'height' => 600,
+        'zoom'  => 10
+    ];
+
+    $params = shortcode_atts($default_value, $attributes);
+
+    $gmap = <<<GMAP
+        <iframe width="{$params['width']}" height="{$params['height']}"
+	src="https://maps.google.com/maps?q={$params['place']}&t=&z={$params['zoom']}&ie=UTF8&iwloc=&output=embed"
+	frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+</iframe>
+GMAP;
+return $gmap;
+
+}
+add_shortcode('gmap', 'easy_gmap_register_shortcode');
